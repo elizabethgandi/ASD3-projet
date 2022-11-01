2714fa6e220997293128f858509d573c43893e00
elizabethgandi <85548696+elizabethgandi@users.noreply.github.com>
2022-11-01 12:57:11 +0100

    Add files via upload
    add class

---
+++ Image.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.awt.*;
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+/**
+ * Helper class to create, modify and save images.
+ * @author Mathieu Vavrille
+ * For example:
+ * ```java
+ * Image img = new Image (100, 200);
+ * img.setRectangle(10, 20, 50, 30, Color.YELLOW); // Color should be imported with `import java.awt.Color`
+ * img.save("test.png");
+ * ```
+ */
+public class Image
+{
+    private final BufferedImage image; // The image
+
+    /** Constructs an empty image (initially black) of width `width` and height `height` */
+    public Image(int width, int height) {
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    }
+
+    /**
+     * Sets one pixel of the image.
+     * WARNING : NO CHECK IS DONE. IF YOU WRITE OUTSIDE THE IMAGE IT WILL RAISE AN ERROR
+     */
+    public void setPixel(int x, int y, java.awt.Color col) {
+        image.setRGB(x,y,col.getRGB());
+    }
+    /**
+     * Sets all the pixels in the given region to the given color.
+     * WARNING : END COORDINATES EXCLUDED.
+     * WARNING : NO CHECK IS DONE. IF YOU WRITE OUTSIDE THE IMAGE IT WILL RAISE AN ERROR
+     */
+    public void setRectangle(int startX, int endX, int startY, int endY, Color color) {
+        for(int x = startX; x < endX; x++) {
+            for(int y = startY; y < endY; y++) {
+                setPixel(x,y,color);
+            }
+        }
+    }
+
+    /**
+     * Saves the image to a file, in PNG format
+     */
+    public void save(String filename) throws IOException {
+        File fic = new File(filename);
+        fic = new File(fic.getAbsolutePath());
+        ImageIO.write(image,"png",fic);
+    }
+
+    /**
+     * Number of pixels in X dimension
+     */
+    public int width() {
+        return image.getWidth();
+    }
+
+    /**
+     * Number of pixels in Y dimension
+     */
+    public int height() {
+        return image.getHeight();
+    }
+}
