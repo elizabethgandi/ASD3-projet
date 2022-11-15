8d456fa0ec38bae55c62464d0729a20fc56b3e58
elizabethgandi <gndblx@yahoo.com>
2022-11-15 10:30:43 +0100

    fusion of AVL and Node class

--- AVL.java
+++ AVL.java
@@ -1,235 +1,271 @@
 public class AVL {
-    public Node root;
+    
     private kdTree pointer ; 
-
-    /* Method which print AVL */
-    public void printAVL(Node nodeOfAVLTest) {
+    public Node root;
+    private int information;
+    private int valueOfTheBalance;
+    private int valueOfTheHeight;
+    private AVL rightSon;
+    private AVL leftSon;
+    
+    
+    public AVL(int information , int valueOfTheHeight){
+        this.information       = information;
+        this.valueOfTheHeight  = valueOfTheHeight;
+    }
+    
+    
+    public int getInformation() {
+        return information;
+    }
+    
+    public void setInformation(int information) {
+        this.information = information;
+    }
+    
+    public int getValueOfTheBalance() {
+        return valueOfTheBalance;
+    }
+    
+    public void setValueOfTheBalance(int valueOfTheBalance) {
+        this.valueOfTheBalance = valueOfTheBalance;
+    }
+    
+    public int getValueOfTheHeight() {
+        return valueOfTheHeight;
+    }
+    
+    public void setValueOfTheHeight(int valueOfTheHeight) {
+        this.valueOfTheHeight = valueOfTheHeight;
+    }
+    
+    public AVL getRightSon() {
+        return this.rightSon;
+    }
+    
+    public void setRightSon(AVL rightSon) {
+        this.rightSon = rightSon;
+    }
+    
+    public AVL getLeftSon() {
+        return this.leftSon;
+    }
+    public void setLeftSon(AVL leftSon) {
+        this.leftSon = leftSon;
+    }
+    
+        /* Method which print AVL */
+    public void printAVL(AVL nodeOfAVLTest) {
 
         if (nodeOfAVLTest != null) {
+            printAVL(nodeOfAVLTest.getLeftSon());
             System.out.print(nodeOfAVLTest.getInformation() + " ");
-            printAVL(nodeOfAVLTest.leftSon);
-            printAVL(nodeOfAVLTest.rightSon);
+            printAVL(nodeOfAVLTest.getRightSon());
         }
-
     }
-
+    
     /* Method who return the maximum of two values enter in parameters */
     public int maximumOfTwoValues(int firstValue, int secondValue){
 
         if(firstValue > secondValue)
             return firstValue;
-
-        return secondValue;
+        
+            return secondValue;
     }
-
-    /* Method who return the height of a node */
-    public int heightValueOfANode(Node node) {
+    
+        /* Method who return the height of a node */
+    public int heightValueOfANode(AVL node) {
         if (node == null)
             return 0;
-
+    
         return node.getValueOfTheHeight();
     }
-
+    
     /* Method who make a right rotation */
-    public Node makeARightRotate(Node nodeRotation){
-        //
-        Node subTreeLeft               = nodeRotation.leftSon;
-        Node subTreeRightOfSubTreeLeft = subTreeLeft.rightSon;
-
-        subTreeLeft.rightSon = nodeRotation;
-        nodeRotation.leftSon = subTreeRightOfSubTreeLeft;
-
-        nodeRotation.valueOfTheHeight = maximumOfTwoValues(heightValueOfANode(nodeRotation.leftSon), heightValueOfANode(nodeRotation.rightSon)) + 1;
-        subTreeLeft.valueOfTheHeight  = maximumOfTwoValues(heightValueOfANode(subTreeLeft.leftSon) , heightValueOfANode(subTreeLeft.rightSon))  + 1;
-
+    public AVL makeARightRotate(AVL nodeRotation){
+            //
+        AVL subTreeLeft               = nodeRotation.getLeftSon();
+        AVL subTreeRightOfSubTreeLeft = subTreeLeft.getRightSon();
+    
+        subTreeLeft.setRightSon(nodeRotation);
+        nodeRotation.setLeftSon(subTreeRightOfSubTreeLeft);
+    
+        nodeRotation.setValueOfTheHeight(maximumOfTwoValues(heightValueOfANode(nodeRotation.leftSon), heightValueOfANode(nodeRotation.rightSon)) + 1);
+        subTreeLeft.setValueOfTheHeight(maximumOfTwoValues(heightValueOfANode(subTreeLeft.leftSon) , heightValueOfANode(subTreeLeft.rightSon))  + 1);
+    
         /* subTreeLeft is now the new root */
         return subTreeLeft;
     }
-
+    
     /* Method who make a left rotation */
-    public Node makeALeftRotate(Node nodeRotation){
-        Node subTreeLeft               = nodeRotation.rightSon;
-        Node subTreeRightOfSubTreeLeft = subTreeLeft.leftSon;
-
-        subTreeLeft.leftSon   = nodeRotation;
-        nodeRotation.rightSon = subTreeRightOfSubTreeLeft;
-
-        nodeRotation.valueOfTheHeight = maximumOfTwoValues(heightValueOfANode(nodeRotation.leftSon), heightValueOfANode(nodeRotation.rightSon)) + 1;
-        subTreeLeft.valueOfTheHeight  = maximumOfTwoValues(heightValueOfANode(subTreeLeft.leftSon) , heightValueOfANode(subTreeLeft.rightSon))  + 1;
-
+    public AVL makeALeftRotate(AVL nodeRotation){
+        AVL subTreeLeft               = nodeRotation.getRightSon();
+        AVL subTreeRightOfSubTreeLeft = subTreeLeft.getLeftSon();
+    
+        subTreeLeft.setLeftSon(nodeRotation);
+        nodeRotation.setRightSon(subTreeRightOfSubTreeLeft);
+    
+        nodeRotation.setValueOfTheHeight (maximumOfTwoValues(heightValueOfANode(nodeRotation.leftSon), heightValueOfANode(nodeRotation.rightSon)) + 1);
+        subTreeLeft.setValueOfTheHeight (maximumOfTwoValues(heightValueOfANode(subTreeLeft.leftSon) , heightValueOfANode(subTreeLeft.rightSon))  + 1);
+    
         /* subTreeLeft is now the new root */
         return subTreeLeft;
     }
-
-    public Node sensRotationAVL(int balanceValue, int informationAdd, Node nodeAdd){
+    
+    public AVL sensRotationAVL(int balanceValue, int informationAdd, AVL nodeAdd){
         if (balanceValue > 1){
-            if (informationAdd > nodeAdd.leftSon.getInformation()){
+            if (informationAdd > nodeAdd.getLeftSon().getInformation()){
                 /* DROTG */
-                nodeAdd.leftSon = makeALeftRotate(nodeAdd.leftSon);
+                nodeAdd.setLeftSon(makeALeftRotate(nodeAdd.getLeftSon()));
                 return makeARightRotate(nodeAdd);
             }
             else /*ROTG */
                 return makeARightRotate(nodeAdd);
         }
         if (balanceValue < -1){
-            if (informationAdd < nodeAdd.rightSon.getInformation()){
+            if (informationAdd < nodeAdd.getRightSon().getInformation()){
                 /* DROTD */
-                nodeAdd.rightSon = makeARightRotate(nodeAdd.rightSon);
+                nodeAdd.setRightSon(makeARightRotate(nodeAdd.getRightSon()));
                 return makeALeftRotate(nodeAdd);
             }
             else /*ROTD */
                 return makeALeftRotate(nodeAdd);
         }
-
+    
         return nodeAdd;
     }
-
-    public Node insertNode(Node nodeAdd, int informationAdd){
-
+    
+    public AVL insertNode(AVL nodeAdd, int informationAdd){
+    
         if (nodeAdd == null) {
-            Node newNode = new Node(informationAdd,0);
+            AVL newNode = new AVL(informationAdd,0);
             return newNode;
         }
-
+    
         else if (informationAdd>nodeAdd.getInformation()){
-            nodeAdd.rightSon = addNewNode(nodeAdd.getRightSon(), informationAdd);
+            nodeAdd.setRightSon(addNewNode(nodeAdd.getRightSon(), informationAdd));
         }
-
+    
         else if (informationAdd<=nodeAdd.getInformation()) {
-            nodeAdd.leftSon = addNewNode(nodeAdd.getLeftSon(), informationAdd);
+            nodeAdd.setLeftSon(addNewNode(nodeAdd.getLeftSon(), informationAdd));
         }
-
+    
         else {
-
+    
             return nodeAdd;
         }
         return nodeAdd;
     }
-
-    public Node addNewNode(kdTree nodeAdd, double informationAdd) {
-
+    
+    public AVL addNewNode(AVL nodeAdd, int informationAdd) {
+    
         if (nodeAdd == null)
-            return new Node(informationAdd, 0);
-
-        Node nodeAddResult;
-
+            return new AVL(informationAdd, 0);
+    
+        AVL nodeAddResult;
+    
         /*Insert the node into the AVL tree with a simple way*/
-
+    
         nodeAdd = insertNode(nodeAdd, informationAdd);
-
+    
         /*Update heights of parent's nodeAdd*/
-
+    
         nodeAdd.setValueOfTheHeight( 1 + maximumOfTwoValues(heightValueOfANode(nodeAdd.getLeftSon()),heightValueOfANode(nodeAdd.getRightSon())));
-
+    
         /*Update balances of nodes in the tree*/
-
+    
         int balanceValue = nodeAdd.getValueOfTheBalance();
-
+    
         /* If the balance = 2 or -2 it's mean that the tree is unbalanced */
-
+    
         nodeAddResult = sensRotationAVL(balanceValue, informationAdd, nodeAdd);
         return nodeAddResult;
     }
-
-    public Node minimumInRightSubTree(Node root){
-        Node minNodeValue = root;
-
-        /* Commme l'AVL est trié on est sur que la plus petite valeure est dans la branche tout à gauche*/
-        while(minNodeValue.leftSon != null )
-            minNodeValue = minNodeValue.leftSon;
-
+    
+    public AVL minimumInRightSubTree(AVL root){
+        AVL minNodeValue = root;
+    
+        /* Commme l'AVL est trié on est sur que la plus petite valeur est dans la branche tout à gauche*/
+        while(minNodeValue.getLeftSon() != null )
+            minNodeValue = minNodeValue.getLeftSon();
+    
         return minNodeValue;
     }
-
-    public Node max(){
-        return maximumInRightSubTree(this);
-    }
-
-    public Node maximumInRightSubTree(Node root){
-        Node maxNodeValue = root;
-
-        /* Commme l'AVL est trié on est sur que la plus grande valeure est dans la branche tout à droite*/
-        while(maxNodeValue.rightSon != null )
-            maxNodeValue = maxNodeValue.rightSon;
-
-        return maxNodeValue;
-    }
-
-
-    public Node removeNodeGiven (Node rootNodeRemove, int valueToRemove){
-
-        Node newNode = null;
-
+    
+    public AVL removeNodeGiven (AVL rootNodeRemove, int valueToRemove){
+    
+        AVL newNode = null;
+    
         // way into the tree
         //value < value of the root
         if (valueToRemove < rootNodeRemove.getInformation())
-            rootNodeRemove.leftSon = removeNodeGiven(rootNodeRemove.leftSon, valueToRemove);
-
-        //value > value of the root
+            rootNodeRemove.setLeftSon(removeNodeGiven(rootNodeRemove.getLeftSon(), valueToRemove));
+    
+            //value > value of the root
         else if (valueToRemove > rootNodeRemove.getInformation())
-            rootNodeRemove.rightSon = removeNodeGiven(rootNodeRemove.rightSon, valueToRemove);
-
-        // the node deleted is the root
-        // the value who has to be deleted is the value of the root of a tree (1), of a leaf (2)
+            rootNodeRemove.setRightSon(removeNodeGiven(rootNodeRemove.getRightSon(), valueToRemove));
+    
+            // the node deleted is the root
+            // the value who has to be deleted is the value of the root of a tree (1), of a leaf (2)
         else {
-
+    
             /*
-             if the node has one child (1)
+            if the node has one child (1)
                   4
                 /
                1
              */
+    
+            if ((rootNodeRemove.getLeftSon() == null) || (rootNodeRemove.getRightSon() == null)) {
 
-            if ((rootNodeRemove.leftSon == null) || (rootNodeRemove.rightSon == null)) {
+               if (rootNodeRemove.getLeftSon() == null)
+                    newNode = rootNodeRemove.getRightSon();
 
-                if (rootNodeRemove.leftSon == null)
-                    newNode = rootNodeRemove.rightSon;
-
-                if (rootNodeRemove.rightSon == null)
-                    newNode = rootNodeRemove.leftSon;
-
-                /* if the node is a leaf
-                        4
-                       / \
-                      ^   ^
-                */
+               if (rootNodeRemove.getRightSon() == null)
+                    newNode = rootNodeRemove.getLeftSon();
+    
+                    /* if the node is a leaf
+                            4
+                           / \
+                          ^   ^
+                    */
                 if (newNode == null) {
                     newNode = rootNodeRemove;
                     rootNodeRemove = null;
-
+    
                 } else
                     rootNodeRemove = newNode;
-            }
-                else {
-                    Node newRoot = minimumInRightSubTree(rootNodeRemove.rightSon);
-
+                }
+            else {
+                AVL newRoot = minimumInRightSubTree(rootNodeRemove.getRightSon());
+    
                 rootNodeRemove.setInformation(newRoot.getInformation());
-
-                rootNodeRemove.rightSon = removeNodeGiven(rootNodeRemove.rightSon, newRoot.getInformation());
+    
+                rootNodeRemove.setRightSon(removeNodeGiven(rootNodeRemove.getRightSon(), newRoot.getInformation()));
             }
         }
         return rootNodeRemove;
     }
-
-    public Node removeNode (Node rootNodeRemove, int valueToRemove){
-
+    
+    public AVL removeNode (AVL rootNodeRemove, int valueToRemove){
+    
         if (rootNodeRemove == null)
             return null; // because rootNodeRemove is always equals to null
-
-        // replace the older root by the new one
+    
+           // replace the older root by the new one
         rootNodeRemove = removeNodeGiven(rootNodeRemove, valueToRemove);
-
+    
         if (rootNodeRemove == null)
             return null; // because rootNodeRemove is always equals to null
-
+    
         // modifie the height of the root in take the maximum of the height of his two sons
-        rootNodeRemove.setValueOfTheHeight(maximumOfTwoValues(rootNodeRemove.leftSon.getValueOfTheHeight(), rootNodeRemove.rightSon.getValueOfTheHeight()) + 1);
-
+        rootNodeRemove.setValueOfTheHeight(maximumOfTwoValues(rootNodeRemove.getLeftSon().getValueOfTheHeight(), rootNodeRemove.getRightSon().getValueOfTheHeight()) + 1);
+    
         //gérer la balance
         int balanceValue = rootNodeRemove.getValueOfTheBalance();
-
+    
         rootNodeRemove = sensRotationAVL(balanceValue, valueToRemove, rootNodeRemove);
-
+    
         return rootNodeRemove;
     }
 
